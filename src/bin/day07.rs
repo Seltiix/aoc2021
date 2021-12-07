c62b6d23fb3b152b91075f8d4d6d6d4278ede644
Filip Klang <filkl010@student.liu.se>
2021-12-07 20:17:36 +0100

    Solved Day 7

---
+++ src/bin/day07.rs
@@ -0,0 +1,60 @@
+#![feature(int_abs_diff)]
+use std::fs;
+
+fn parse_input(data_as_string: String) -> Vec<u32> {
+    data_as_string.split(',').map(|digit| digit.parse().unwrap()).collect()
+}
+
+fn main() {
+    let data = fs::read_to_string("inputs/day7/day07.txt").expect("Could not read file");
+    let crabs = parse_input(data);
+
+    println!("Solution to Day 7, part 1: {}", part1(crabs.clone()));
+    println!("Solution to Day 7, part 2: {}", part2(crabs));
+}
+
+fn part1(mut crabs: Vec<u32>) -> u32 {
+    crabs.sort();
+    let median = crabs[crabs.len()/2]; // Median is (x + x1)/2 for even length. For me it was not
+
+    crabs.iter().fold(0, |sum, crab|  sum + crab.abs_diff(median))
+}
+
+fn part2(crabs: Vec<u32>) -> u32 {
+    let sum: u32 = crabs.iter().sum();
+    let average = (sum as f32 / crabs.len() as f32) as u32;
+    //let average = (sum as f32 / crabs.len() as f32).round as u32;
+
+    let mut fuel_cost = 0;
+    for crab in crabs {
+        let diff = crab.abs_diff(average);
+
+        for i in 1..=diff {
+            fuel_cost += i
+        }
+
+    }
+
+    fuel_cost
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_1_example() {
+        let data = fs::read_to_string("inputs/day7/day07_small.txt").expect("Could not read file");
+        let crabs = parse_input(data);
+
+        assert_eq!(part1(crabs), 37);
+    }
+
+    #[test]
+    fn part_2_example() {
+        let data = fs::read_to_string("inputs/day7/day07_small.txt").expect("Could not read file");
+        let crabs = parse_input(data);
+
+        assert_eq!(part2(crabs), 168);
+    }
+}
